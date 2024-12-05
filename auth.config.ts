7134748862bb3f81462d3eed85a2576fa02696ba
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2024-12-05 09:25:53 +0100

    Update auth.config.ts to include clientId and clientSecret for Google and GitHub providers

--- auth.config.ts
+++ auth.config.ts
@@ -3,7 +3,16 @@ import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 
 export const authConfig = {
-  providers: [Google, GitHub],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
   callbacks: {
     async jwt({ token, user }) {
       if (user?.id) {
