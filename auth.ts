126777a7a9a43c687d47aa5576bddbb963f8ded4
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2024-12-08 13:42:25 +0100

    Enhance authentication configuration by adding trustHost setting for production environment

--- auth.ts
+++ auth.ts
@@ -3,8 +3,11 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import { authConfig } from "./auth.config";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
+  trustHost: isProduction,
   ...authConfig,
 });
